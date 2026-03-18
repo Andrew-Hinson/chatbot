7a8079ed72ff819cde780184c00feac634514e48
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-03-18 16:24:14 -0500

    key made and retrieval checked

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+
 def main():
+    load_dotenv()
+    api_key = ""
+    if "GEMENI_API_KEY" in os.environ:
+        api_key = os.environ.get("GEMENI_API_KEY")
+        print("gemeni key retrieved")
+    else:
+        raise Exception("Gemeni API Key not found")
+
     print("Hello from chatbot!")
 
 
