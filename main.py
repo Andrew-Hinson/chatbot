b74c1db4dad5d02165505024665c6161a9763f86
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-03-18 16:37:20 -0500

    reading response token metadata

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ def main():
 
     client = genai.Client(api_key=api_key)
     content = client.models.generate_content(model="gemini-2.5-flash", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+
+    if content.usage_metadata is not None:
+        print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+    else:
+        raise Exception("Cannot read usage metadata")
     print(content.text)
 
 
