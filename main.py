b3b6e5ec446de8456f7152112d79c09c456ec785
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-03-19 20:28:31 -0500

    refactor and added verbose mode

--- main.py
+++ main.py
@@ -14,19 +14,33 @@ def main():
     else:
         raise Exception("Gemeni API Key not found")
 
-    client = genai.Client(api_key=api_key)
+    
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
-    response = client.models.generate_content(model="gemini-2.5-flash", contents= messages)
 
-    if response.usage_metadata is not None:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    else:
-        raise Exception("Cannot read usage metadata")
+    client = genai.Client(api_key=api_key)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+    generate_content(client, messages, args.verbose)
+
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            )
+    if not response.usage_metadata:
+        raise RuntimeError("Gemini API reponse appears to be malformed")
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    print("Response:")
     print(response.text)
 
 
