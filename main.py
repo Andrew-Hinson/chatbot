472b247cabc805d1f7028197b9057a0014a99b8f
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-03-19 20:31:46 -0500

    spelling

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from google.genai import types
 def main():
     load_dotenv()
     api_key = ""
-    if "GEMENI_API_KEY" in os.environ:
-        api_key = os.environ.get("GEMENI_API_KEY")
-        print("gemeni key retrieved")
+    if "GEMINI_API_KEY" in os.environ:
+        api_key = os.environ.get("GEMINI_API_KEY")
+        print("gemini key retrieved")
     else:
-        raise Exception("Gemeni API Key not found")
+        raise Exception("Gemini API Key not found")
 
     
 
