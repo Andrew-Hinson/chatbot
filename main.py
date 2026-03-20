1ffc68f44606fb368033054186a9f1d804598dbf
William A Hinson <73917662+Andrew-Hinson@users.noreply.github.com>
2026-03-19 20:08:21 -0500

    using types to keep track of message history

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -18,14 +19,15 @@ def main():
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
-    content = client.models.generate_content(model="gemini-2.5-flash", contents= args.user_prompt)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(model="gemini-2.5-flash", contents= messages)
 
-    if content.usage_metadata is not None:
-        print(f"Prompt tokens: {content.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {content.usage_metadata.candidates_token_count}")
+    if response.usage_metadata is not None:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
         raise Exception("Cannot read usage metadata")
-    print(content.text)
+    print(response.text)
 
 
 if __name__ == "__main__":
